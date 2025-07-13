50e4075711c8cc943e48d71abd960a0feb56b25f
Brennan Sauer <bsauer02@Brennans-MacBook-Pro.local>
2025-07-13 01:08:58 -0500

    Frontend: Complete React app setup with AppFlow component

---
+++ rp-exotics-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppFlow from "./components/AppFlow";
+
+function App() {
+  return (
+    <div className="App">
+      <AppFlow />
+    </div>
+  );
+}
+
+export default App;
